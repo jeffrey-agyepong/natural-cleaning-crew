9e6da5846f64eefc82a8aeb35d862c60dce5e032
Jeffrey777A <44594550+Jeffrey777A@users.noreply.github.com>
2020-11-18 03:32:07 +0000

    hamburger menu(open & close)

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,14 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const fadeElm = document.querySelectorAll('.has-fade');
+
 
 btnHamburger.addEventListener('click', function(){
   
 console.log('open hamburger');
-if(header.classList.contains(open))
+
+if(header.classList.contains('open'))
  //Close Hamburger Menu
 {
   header.classList.remove('open');
+  fadeElm.forEach(function(element){
+    element.classList.remove('fade-in');
+    element.classList.add('fade-out');
+  })
 }  else {
   header.classList.add('open');
+  fadeElm.forEach(function(element){
+    element.classList.remove('fade-out');
+    element.classList.add('fade-in');
+  })
 }
 });
